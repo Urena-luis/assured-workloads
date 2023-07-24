a6f64ed72d972981d06d6e96f9a30138da44bc6b
Urena-luis <80923410+Urena-luis@users.noreply.github.com>
2023-07-24 14:50:49 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -17,16 +17,15 @@
 
 ## NOTE: This provides PoC demo environment for Assured Workload ##
 ##  This is not built for production workload ##
-
 variable "organization_id" {
   description = "(Required)" #The organization for the resource
   type        = string
   default     = "XXXXXXXX"
 }
 
-
+#Required. Input only. The billing account used for the resources which are direct children of workload. This billing account is initially associated with the resources created as part of Workload creation. After the initial creation of these resources, the customer can change the assigned billing account. The resource name has the form `billingAccounts/{billing_account_id}`. For example, 'billingAccounts/012345-567890-ABCDEF`.
 variable "billing_account" {
-  description = "(Required)" #Required. Input only. The billing account used for the resources which are direct children of workload. This billing account is initially associated with the resources created as part of Workload creation. After the initial creation of these resources, the customer can change the assigned billing account. The resource name has the form `billingAccounts/{billing_account_id}`. For example, 'billingAccounts/012345-567890-ABCDEF`.
+  description = "(Required)" 
   type        = string
   default     = "XXXXX-XXXXX-XXXXXX"
 }
@@ -38,53 +37,51 @@ variable "members" {
   default     = ["user:username@domain.com"]
 }
 
+#Required. The user-assigned display name of the Workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
 variable "aw_mgmt_project_id" {
-  description = "(Required)" #Required. The user-assigned display name of the Workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
+  description = "(Required)" 
   type        = string
-  default     = "aw-mgmt-us-"
+  default     = "aw-mgmt-australia"
 }
 
-
-
-
+ #Required. The user-assigned display name top folder for application/department/workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
 variable "app_folder_name" {
-  description = "(Required)" #Required. The user-assigned display name top folder for application/department/workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
+  description = "(Required)"
   type        = string
-  default     = "CSA SpringBoard AW"
+  default     = "Australia Data Residency"
 }
 
-
-
-
+#Required. The user-assigned display name of the Workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
 variable "assured_workloads_workload_display_name" {
-  description = "(Required)" #Required. The user-assigned display name of the Workload. When present it must be between 4 to 30 characters. Allowed characters are: lowercase and uppercase letters, numbers, hyphen, and spaces. Example: My Workload
+  description = "(Required)" 
   type        = string
-  default     = "CSA Assured Workload - IL4"
+  default     = "Australia Data Residency"
 }
 
+#Required. Immutable. Compliance Regime associated with this workload. Possible values: COMPLIANCE_REGIME_UNSPECIFIED, IL4, CJIS, FEDRAMP_HIGH, FEDRAMP_MODERATE, US_REGIONAL_ACCESS
 variable "assured_workloads_workload_compliance_regime" {
-  description = "(Required)" #Required. Immutable. Compliance Regime associated with this workload. Possible values: COMPLIANCE_REGIME_UNSPECIFIED, IL4, CJIS, FEDRAMP_HIGH, FEDRAMP_MODERATE, US_REGIONAL_ACCESS
+  description = "AU_REGIONS_AND_SUPPORT" 
   type        = string
-  default     = "IL4" #EU_REGIONS_AND_SUPPORT, US_REGIONAL_ACCESS, COMPLIANCE_REGIME_UNSPECIFIED, IL4, CJIS, FEDRAMP_HIGH,FEDRAMP_MODERATE, US_REGIONAL_ACCESS, HIPAA, HITRUST, CA_REGIONS_AND_SUPPORT, ITAR, AU_REGIONS_AND_US_SUPPORT, ASSURED_WORKLOADS_FOR_PARTNERS
+
+#Available Options: EU_REGIONS_AND_SUPPORT, US_REGIONAL_ACCESS, COMPLIANCE_REGIME_UNSPECIFIED, IL4, CJIS, FEDRAMP_HIGH,FEDRAMP_MODERATE, US_REGIONAL_ACCESS, HIPAA, HITRUST, CA_REGIONS_AND_SUPPORT, ITAR, AU_REGIONS_AND_US_SUPPORT, ASSURED_WORKLOADS_FOR_PARTNERS
+  default     = "AU_REGIONS_AND_SUPPORT" 
 }
 
 variable "assured_workloads_workload_location" {
   description = "(Required)" #The location for the resource
   type        = string
-  default     = "us" # either a single region or country code
+  default     = "australia-southeast1" # either a single region or country code
   }
 
   variable "assured_workloads_label" {
-  description = "(Required)" #The location for the resource
+  description = "Australia" #The location for the resource
   type        = string
-  default     = "aw-il4" # either a single region or country code
+  default     = "aw-australia" # either a single region or country code
   }
-
-
 
 variable "network_region" {
   type    = string
-  default = "us-west1"
+  default = "australia-southeast1"
 }
 
 variable "vpc_network_name" {
@@ -94,9 +91,8 @@ variable "vpc_network_name" {
 
 variable "network_zone" {
   type    = string
-  default = "us-west1-b"
+  default = "australia-southeast1-b"
 }
-
 
 variable "crypto_key_name" {
   type    = string
@@ -106,52 +102,4 @@ variable "crypto_key_name" {
 variable "key_ring_name" {
   type    = string
   default = "ring"
-}
-
-
-
-variable "policy_name" {
-  description = "The policy's name."
-  type        = string
-  default     = "automatic_folder"
-}
-
-
-
-
-
-variable "perimeter_name" {
-  description = "Name of perimeter."
-  type        = string
-  default     = "regular_perimeter"
-}
-
-variable "restricted_services" {
-  description = "List of services to restrict."
-  type        = list(string)
-  default = [
-    "storage.googleapis.com",
-    "logging.googleapis.com",
-    "compute.googleapis.com",
-    "bigquery.googleapis.com",
-  ]
-}
-
-variable "new_services" {
-  description = "List of new services to restrict that are recently approved to assured workloads but not allowed default"
-  type        = set(string)
-  default = [
-    "bigquery.googleapis.com",
-  ]
-}
-
-
-variable "enforced_regional_access" {
-    description = "CountryRegion "
-    type = list(string)
-    default = [
-        "US", # USA
-        "CA", # Canada
-        "UM", #  US Minor Outlying Islands
-        ]
 }
